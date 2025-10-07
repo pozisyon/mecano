37e85f4c20bfd8df0bbaea979a6b9f8b4dd00986
Kevin Nimi <k.nimi73@gmail.com>
2025-10-07 17:35:11 -0400

    installation de sprintboot

---
+++ annuaire/src/test/java/com/uqtr/annuaire/AnnuaireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uqtr.annuaire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnuaireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
